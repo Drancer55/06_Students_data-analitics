3dea80b5ca497c3189d1b0bd7ff0ee5b9c17da69
Drancer55 <antonio281255@gmail.com>
2021-11-23 15:01:55 -0600

    ocultando pantallas

---
+++ src/index.js
@@ -0,0 +1,4 @@
+document.getElementById('pantalla1').hidden = false;
+document.getElementById('pantalla2').hidden = true;
+document.getElementById('pantalla3').hidden = true;
+document.getElementById('pantalla4').hidden = true;
