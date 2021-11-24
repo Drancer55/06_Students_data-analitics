3f4d80c75e5a4b2c844d6c44144a03055827a798
FernandaFA02 <fajardo.al9402@gmail.com>
2021-11-23 18:26:17 -0600

    agregando login a pantalla1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-document.getElementById('pantalla1').hidden = false;
-document.getElementById('pantalla2').hidden = true;
-document.getElementById('pantalla3').hidden = true;
-document.getElementById('pantalla4').hidden = true;
+document.getElementById('pantalla-1').hidden = false;
+document.getElementById('pantalla-2').hidden = true;
+document.getElementById('pantalla-3').hidden = true;
+
+//Se crearan variables para obtener los valores del input de usuario y password
+/*Se establecen las condiciones para dejar entrar al usuario, si son correctas se oculta la pantalla 
+1 y se muestra la pantalla dos*/
+
+let entrada = document.getElementById('entrar')
+entrada.addEventListener('click', () => {
+    let user = document.getElementById('email').value 
+    let password = document.getElementById('contraseña').value
+    if(user == 'usuario.name@bootCampSchool.com' && password == 12345)
+    document.getElementById('pantalla-1').hidden = true
+    document.getElementById('pantalla-2').hidden = false
+})
