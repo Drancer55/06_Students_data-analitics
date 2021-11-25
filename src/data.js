9f180c548cb7abaf1fb3e7cf4f32f56c3e06de04
FernandaFA02 <fajardo.al9402@gmail.com>
2021-11-25 12:28:43 -0600

    probando entrada a git

--- src/data.js
+++ src/data.js
@@ -3,15 +3,3 @@ fetch("../data/students.json")
 .then((data) => iterarDataSedes(data))
 .catch((error) => console.log(error) )
 
-let sedes = document.getElementById("sedes")
-
-let iterarDataSedes = (data) => {
-    console.log(data.ajusco)
-
-    for (const sede of data.ajusco) {
-        console.log(data.ajusco)
-
-        
-    }
-
-}
