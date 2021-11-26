b96df3a058f25e0bd070f2255ea3592905818b82
Drancer55 <antonio281255@gmail.com>
2021-11-26 12:40:35 -0600

    tratando deo iterar

--- src/data.js
+++ src/data.js
@@ -1,15 +1,1 @@
-fetch("../data/students.json") 
-.then((response) => response.json())
-.then((data) => iterarDataSedes(data, sede, generacion))
-.catch((error) => console.log(error) )
 
-
-
-var iterar = (data) => {
- console.log(data.ajusco)
- let sedes = document.getElementById("sedes")
-for (let i = 0; i < data.ajusco.length; i++) {
-    
-    sedes.innerHTML += `<h1> ${data.ajusco[i].generacion} </h1>`
-}
-}
