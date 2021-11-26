7d5346d7f8429bcecab6cd4e9784518ad324025b
getzemani18 <amandaalvarado0129@gmail.com>
2021-11-26 12:22:06 -0600

    iterando1

--- src/data.js
+++ src/data.js
@@ -3,21 +3,21 @@ fetch("../data/students.json")
 .then((response) => response.json())
 .then((data) => iterarDataSedes(data,sede))
 .catch((error) => console.log(error) )
-}
+
 
 let sedes = document.getElementById("sedes")
 
  var iterarDataSedes = (data, sede) => {
     console.log(data.ajusco)
     for (let i= 0; i< data.ajusco.length; i++) {
-        const sedes = sede[i];
+        const sede = sede[i];
         
 
         sedes.innerHTML+= `<h1> ${data.sedes.generacion} </h1>`
     }
     
 }
-
+}
 // var iterar = (data) => {
 //  console.log(data.ajusco)   
  
