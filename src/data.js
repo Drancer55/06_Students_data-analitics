950b32f2e4c3d596841f5ad31c102021aa5ff4fe
getzemani18 <amandaalvarado0129@gmail.com>
2021-11-30 21:18:37 -0600

    .

--- src/data.js
+++ src/data.js
@@ -29,57 +29,10 @@ fetch(Data)
 .catch((error) => console.log(error))
 }
 //Se crea una funcion pura para iterar datos por sede
-<<<<<<< HEAD
-let iterarData = (data) => {
-    console.log(data);
-    arrBruto.push(data)
-    console.log(arrBruto);
-}
-//arr = rreglo en bruto
-const traerGeneracion = (arrBruto, sede) => {
-    console.log(arrBruto[0][sede])   
-    //pintar generaciones por cada una de las sedes
-    for (const key in arrBruto) {
-            const element = arrBruto[key];
-            console.log(arrBruto[0][sede].generacion);
-            
-            
-        }
-    }
-
-//Datos Ajusco, al dar click en el boton de entrada se atraen los datos de esta sede
-let ajusco = document.getElementById('ajusco')
-ajusco.addEventListener('click', function (){
-    console.log("click en: ", ajusco)
-    traerGeneracion(arrBruto, "ajusco") //prendemos la funcion y con ayuda de ella atraemos los datos de esta sede
-})
-
-
-//Datos Chapultepec, al dar click en el boton de entrada se atraen los datos de esta sede
-let chapultepec = document.getElementById('chapultepec')
-chapultepec.addEventListener('click', function(){
-    console.log("click en: ", chapultepec)
-    traerGeneracion(arrBruto, "chapultepec")//prendemos la funcion y con ayuda de ella atraemos los datos de esta sede
-})
-
-//Datos para iztapalapa, al dar click en el boton de entrada se atraen los datos de esta sede
-let iztapalapa= document.getElementById('iztapalapa')
-iztapalapa.addEventListener('click', function(){
-    console.log("click en: ", iztapalapa)
-    traerGeneracion(arrBruto, "iztapalapa")//prendemos la funcion y con ayuda de ella atraemos los datos de esta sede
-})
-
-
-const traerGeneracion1 = (arrBruto, sede) => {
-    console.log(arrBruto[0][sede].generacion.primera);
-    
-    for (const key1 in arrBruto[0]) {
-=======
 const mostrarSedes = (dataEscolar) => {
     for (const key in dataEscolar) {
         console.log(key);
         document.getElementById("sedes").innerHTML += `<button onclick="dashBoard.traerSede('${key}')"> ${key} </button>`
->>>>>>> 1cdc3373bd9c92b8286a00bca46eea880bcbf5f4
     }
 }
 
@@ -120,3 +73,5 @@ export const datosEstudiantes = (alumno) => {
     //     document.getElementById("estudiantes").innerHTML = ``
     //     
     //     console.log(dataEstudiantes);
+
+    
