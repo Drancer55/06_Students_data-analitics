256b17efacbb414a143eb0a2771d3a9a25ccf7f2
Drancer55 <antonio281255@gmail.com>
2021-12-10 01:01:25 -0600

    subiendo maquetado

--- src/data.js
+++ src/data.js
@@ -41,11 +41,10 @@ fetch(Data)
 }
             //---------------Funcion dinamica para atraer y pintar los datos de las sedes en pantalla dos--------------//
 const mostrarSedes = (dataEscolar) => {
+  // let grafica1 = document.getElementById('grafica1')
+  // let grafica2 = document.getElementById('grafica2')
+  // let grafica3 = document.getElementById('grafica3')
   let img;
-  let grafica1 = document.getElementById('grafica1')
-  let grafica2 = document.getElementById('grafica2')
-  let grafica3 = document.getElementById('grafica3')
-
   let map;
   let campus;
   let botn;
@@ -57,7 +56,7 @@ const mostrarSedes = (dataEscolar) => {
       campus = "Ajusco";
       map =
         "https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d15069.378607057964!2d-99.19927516935243!3d19.223805575500283!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e1!4m0!4m5!1s0x85cdfdf6a711c163%3A0xc02b29d16232aecf!2sAv.%20La%20Felicidad%20242%2C%20San%20Miguel%20Ajusco%2C%20Tlalpan%2C%2014700%20San%20Miguel%20Ajusco%2C%20CDMX!3m2!1d19.224214999999997!2d-99.194273!5e0!3m2!1ses-419!2smx!4v1637996084351!5m2!1ses-419!2smx";
-      grafica1.innerHTML = `<img src="../assets/graficaAjusco.png" />`
+      //grafica1.innerHTML = `<div class="graficar"><img class="grafica" src="../assets/graficaAjusco.png" /></div>`
  
         //botn = `<button onclick="dashBoard.traerGeneracion('${key}')"> ${key} </button>`
     } else if (key == "chapultepec") {
@@ -65,14 +64,14 @@ const mostrarSedes = (dataEscolar) => {
       campus = "Chapultepec";
       map =
         "https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d15050.963273306652!2d-99.19941515547023!3d19.423601324983508!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e1!4m0!4m5!1s0x85d201f8e7cbce13%3A0xf8bbfda793220ccc!2sCalle%20Julio%20Verne%2027%2C%20Polanco%2C%20Polanco%20IV%20Secc%2C%20Miguel%20Hidalgo%2C%2011560%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!3m2!1d19.427963899999998!2d-99.1969393!5e0!3m2!1ses-419!2smx!4v1637996847042!5m2!1ses-419!2smx";
-        grafica2.innerHTML = `<img src="../assets/graficaChapu.png"/>`
+        //grafica2.innerHTML = `<img src="../assets/graficaChapu.png"/>`
         //botn = `<button onclick="dashBoard.traerGeneracion('${key}')"> ${key} </button>`
     } else if (key == "iztapalapa") {
       img = "../assets/LogoIztapalapa.jpeg";
       campus = "Iztapalapa";
       map =
         "https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d30115.851234696875!2d-99.08672062336888!3d19.348297199433908!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e1!4m0!4m5!1s0x85d1fd88c97e8ce7%3A0x6978b1f508c61541!2sErmita%20Iztapalapa%202001%2C%20Los%20%C3%81ngeles%2C%20Iztapalapa%2C%2009830%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!3m2!1d19.347834499999998!2d-99.0698948!5e0!3m2!1ses-419!2smx!4v1637998520536!5m2!1ses-419!2smx";
-        grafica3.innerHTML = `<img src="../assets/graficaIzta.png"/>`
+        //grafica3.innerHTML = `<img src="../assets/graficaIzta.png"/>`
         //botn = `<button onclick="dashBoard.traerGeneracion('${key}')"> ${key} </button>`
     }
     document.getElementById("sedes").innerHTML += `
@@ -117,9 +116,9 @@ export const iterarGen = (gen) => {
   limpiarEstudiantes();
   document.getElementById("estudiantes").innerHTML = 
   `<h3 onclick="dashBoard.traerEstudiantes('${gen}')">Estudiantes de ${gen} generación: </h3>`;
-  // limpiarBotones();
-  // document.getElementById('buscar').innerHTML += 
-  // `<button onclick="dashBoard.traerGeneracion('${gen}')" class="btn btn-primary">Buscar en ${gen} generación</button>`
+  limpiarBotones();
+  document.getElementById('buscar').innerHTML += 
+  `<button onclick="dashBoard.traerGeneracion('${gen}')" class="btn btn-primary">Buscar en ${gen} generación</button>`
 };
 
        //---------------Se pinta dinamicamente el boton de regresar a la pantalla dos que nos muestra las sedes----------------//
@@ -177,6 +176,7 @@ export const datosEstudiantes = (gen) => {
                 <h6 class="card-text"><b>E-mail:</b> ${arrBruto[0][lugar].generacion[gen].estudiantes[i].correo}</h6>
                 <h6 class="card-text"><b>Duración:</b> ${arrBruto[0][lugar].generacion[gen].estudiantes[i].progreso.duracionPrograma}hrs.</h6>
                 <h6 class="card-text"><b>Progreso completado:</b> ${arrBruto[0][lugar].generacion[gen].estudiantes[i].progreso.porcentajeCompletado}%</h6>
+                <img id="avatar" src="../assets/ImagenCard.jpeg"></img>
                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target=#id${i}>
                     Ver más...
                 </button>
@@ -365,14 +365,12 @@ const porcentajesCompletados = (estudiantesPorcentajes) => {
       if (element.progreso.porcentajeCompletado < 60 ) {
         //console.log("Alumnos debajo del 60%: " + element.nombre );
       //Se atrae la variable antes declarada y se utliza un innerHTML para pintar resultados
-        sesentaMenos.innerHTML = `<h3>Alumnos debajod el 60%: ${element.nombre} ${element.progreso.porcentajeCompletado}</h3>`
+        sesentaMenos.innerHTML = `<h3 class="debajo60">Alumnos debajo de el 60%: ${element.nombre} ${element.progreso.porcentajeCompletado}</h3>`
       }else if (element.progreso.porcentajeCompletado > 90){
         //console.log("Alumnos arriba del 90%: " + element.nombre );
         //Se atrae la variable antes declarada y se utliza un innerHTML para pintar resultados
-        masNoventa.innerHTML = `<h3>Alumnos arriba del 90%: ${element.nombre}${element.progreso.porcentajeCompletado}</h3>`
+        masNoventa.innerHTML = `<h3 class="debajo90">Alumnos arriba del 90%: ${element.nombre}${element.progreso.porcentajeCompletado}</h3>`
         estudiantesPorcentajes = [];
       }
-      
     })
-
   }
