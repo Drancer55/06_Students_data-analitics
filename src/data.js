31fcb09e7f465152b2d77c6af5950fc52a5e983e
FernandaFA02 <fajardo.al9402@gmail.com>
2021-12-09 21:11:27 -0600

    rehajustando graficas

--- src/data.js
+++ src/data.js
@@ -42,9 +42,7 @@ const mostrarSedes = (dataEscolar) => {
   let grafica1 = document.getElementById('grafica1')
   let grafica2 = document.getElementById('grafica2')
   let grafica3 = document.getElementById('grafica3')
-  let grafica4 = document.getElementById('grafica4')
-  let grafica5 = document.getElementById('grafica5')
-  let grafica6 = document.getElementById('grafica6')
+
   let map;
   let campus;
   let botn;
@@ -56,27 +54,22 @@ const mostrarSedes = (dataEscolar) => {
       campus = "Ajusco";
       map =
         "https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d15069.378607057964!2d-99.19927516935243!3d19.223805575500283!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e1!4m0!4m5!1s0x85cdfdf6a711c163%3A0xc02b29d16232aecf!2sAv.%20La%20Felicidad%20242%2C%20San%20Miguel%20Ajusco%2C%20Tlalpan%2C%2014700%20San%20Miguel%20Ajusco%2C%20CDMX!3m2!1d19.224214999999997!2d-99.194273!5e0!3m2!1ses-419!2smx!4v1637996084351!5m2!1ses-419!2smx";
-      grafica1.innerHTML = `<img src="../assets/Grafico1.jpg" />`
-      grafica2.innerHTML = `<img src="../assets/Grafico2.jpg"/>`
-      grafica3.innerHTML = `<img src="../assets/Grafico3.jpg"/>`
+      grafica1.innerHTML = `<img src="../assets/graficaAjusco.png" />`
+ 
         //botn = `<button onclick="dashBoard.traerGeneracion('${key}')"> ${key} </button>`
     } else if (key == "chapultepec") {
       img = "../assets/LogoChapultepec.jpeg";
       campus = "Chapultepec";
       map =
         "https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d15050.963273306652!2d-99.19941515547023!3d19.423601324983508!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e1!4m0!4m5!1s0x85d201f8e7cbce13%3A0xf8bbfda793220ccc!2sCalle%20Julio%20Verne%2027%2C%20Polanco%2C%20Polanco%20IV%20Secc%2C%20Miguel%20Hidalgo%2C%2011560%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!3m2!1d19.427963899999998!2d-99.1969393!5e0!3m2!1ses-419!2smx!4v1637996847042!5m2!1ses-419!2smx";
-        grafica4.innerHTML = `<img src="../assets/Grafico4.jpg"/>`
-        grafica5.innerHTML = `<img src="../assets/Grafico5.jpg"/>`
-        grafica6.innerHTML = `<img src="../assets/Grafico6.jpg"/>`
+        grafica2.innerHTML = `<img src="../assets/graficaChapu.png"/>`
         //botn = `<button onclick="dashBoard.traerGeneracion('${key}')"> ${key} </button>`
     } else if (key == "iztapalapa") {
       img = "../assets/LogoIztapalapa.jpeg";
       campus = "Iztapalapa";
       map =
         "https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d30115.851234696875!2d-99.08672062336888!3d19.348297199433908!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e1!4m0!4m5!1s0x85d1fd88c97e8ce7%3A0x6978b1f508c61541!2sErmita%20Iztapalapa%202001%2C%20Los%20%C3%81ngeles%2C%20Iztapalapa%2C%2009830%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!3m2!1d19.347834499999998!2d-99.0698948!5e0!3m2!1ses-419!2smx!4v1637998520536!5m2!1ses-419!2smx";
-        grafica4.innerHTML = `<img src="../assets/Grafico4.jpg"/>`
-        grafica5.innerHTML = `<img src="../assets/Grafico5.jpg"/>`
-        grafica6.innerHTML = `<img src="../assets/Grafico6.jpg"/>`
+        grafica3.innerHTML = `<img src="../assets/graficaIzta.png"/>`
         //botn = `<button onclick="dashBoard.traerGeneracion('${key}')"> ${key} </button>`
     }
     document.getElementById("sedes").innerHTML += `
